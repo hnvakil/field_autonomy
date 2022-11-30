c340e0008268907797b4cd5d35b7bfbbde7f8af8
Han V <hvakil@olin.edu>
2022-11-30 15:40:06 -0500

    got rid of unused Strings

--- arduino/passthrough/passthrough.ino
+++ arduino/passthrough/passthrough.ino
@@ -1,6 +1,5 @@
 #include <Servo.h>
 #include <math.h>
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -33,7 +32,6 @@ bool printDebugging = true;
 
 const byte numChars = 32;
 char receivedChars[numChars];   // an array to store the received data
-String s_a;
 boolean newData = true;
 float serialInput[2]; //0: linear, 1: angular
 
@@ -106,14 +104,9 @@ void loop() {
   }
 }
 
-String convertToString(char* a){
-  String s = a;
-  return s;
-}
-
 void recvWithEndMarker() {
     static byte ndx = 0;
-    char endMarker = '\n';
+    char endMarker = '>';
     char rc;
     
     while (Serial.available() > 0 && newData == false) {
@@ -134,25 +127,8 @@ void recvWithEndMarker() {
     }
 }
 
-String getValue(String data, char separator, int index)
-{
-    int found = 0;
-    int strIndex[] = { 0, -1 };
-    int maxIndex = data.length() - 1;
-
-    for (int i = 0; i <= maxIndex && found <= index; i++) {
-        if (data.charAt(i) == separator || i == maxIndex) {
-            found++;
-            strIndex[0] = strIndex[1] + 1;
-            strIndex[1] = (i == maxIndex) ? i+1 : i;
-        }
-    }
-    return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
-}
-
 void showNewData() {
     if (newData == true) {
-        //String s_a = convertToString(receivedChars);
         char* velVal = strtok(receivedChars, ",");
         char* firstValChar = velVal;
         velVal = strtok(NULL, ",");
@@ -164,11 +140,6 @@ void showNewData() {
         Serial.print(" | second float: ");
         Serial.println(secondFloat);
 
-        //String firstVal = getValue(s_a, ',', 0);
-        //String secondVal = getValue(s_a, ',', 1);
-        //float firstFloat = firstVal.toFloat();
-        //float secondFloat = secondVal.toFloat();
-        
         serialInput[0] = firstFloat;
         serialInput[1] = secondFloat;
         newData = false;
