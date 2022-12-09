2e487a6e049a5c9f255a23e7fc2609c784d9a8f2
al-li-son <allisonli18710@gmail.com>
2022-12-09 16:38:12 -0500

    Add install and run instructions

--- arkit_data_streamer/launch/launch_data_servers.py
+++ arkit_data_streamer/launch/launch_data_servers.py
@@ -11,7 +11,7 @@ def generate_launch_description():
             package='arkit_data_streamer',
             namespace='pose_server',
             executable='pose_server',
-            name='pose',
+            name='pose_server',
             parameters=[
                 {'port': 35601}
             ]
@@ -20,7 +20,7 @@ def generate_launch_description():
             package='arkit_data_streamer',
             namespace='image_server',
             executable='image_server',
-            name='image',
+            name='image_server',
             parameters=[
                 {'port': 35602}
             ]
@@ -29,7 +29,7 @@ def generate_launch_description():
             package='arkit_data_streamer',
             namespace='gps_server',
             executable='gps_server',
-            name='gps',
+            name='gps_server',
             parameters=[
                 {'port': 35603}
             ]
