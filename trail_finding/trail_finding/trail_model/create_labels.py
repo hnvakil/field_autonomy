079be03069e0b7647aaa0cb8cbcdaa07a7a01411
kviiim <k82019@gmail.com>
2022-12-11 11:27:13 -0500

    update label creation

--- trail_finding/trail_finding/trail_model/create_labels.py
+++ trail_finding/trail_finding/trail_model/create_labels.py
@@ -4,31 +4,27 @@ import pandas as pd
 directory_path = "data"
 dir_list = os.listdir(directory_path)
 
-#lc = 0
-#sc = 1
-#rc = 2
+lc = 0
+sc = 1
+rc = 2
 
-lc_list = []
-rc_list = []
-sc_list = []
+paths_list = []
+labels_list = []
 
-for section in dir_list:
-    for frame in os.scandir(f'data/{section}/videos/lc'):
-        if frame.is_dir():
-            for img in os.scandir(frame.path):
-                lc_list.append(img.path)
-    for frame in os.scandir(f'data/{section}/videos/sc'):
-        if frame.is_dir():
-            for img in os.scandir(frame.path):
-                sc_list.append(img.path) 
-    for frame in os.scandir(f'data/{section}/videos/rc'):
-        if frame.is_dir():
-            for img in os.scandir(frame.path):
-                rc_list.append(img.path)
-
-
-paths_list = lc_list + rc_list + sc_list
-labels_list = [0 for i in range(len(lc_list))] + [2 for i in range(len(rc_list))] + [1 for i in range(len(sc_list))]
+for dirpath, dirnames, filenames in os.walk("./data"):
+    for filename in filenames:
+        if filename.endswith(".jpg") or filename.endswith(".png"):
+            path = os.path.join(dirpath, filename)
+            print(path)
+            if "rc" in path:
+                labels_list.append(2)
+                paths_list.append(path)
+            elif "sc" in path:
+                labels_list.append(1)
+                paths_list.append(path)
+            elif "lc" in path:
+                labels_list.append(0)
+                paths_list.append(path)
 
 df = pd.DataFrame({'path': paths_list, 'label': labels_list}) 
 
